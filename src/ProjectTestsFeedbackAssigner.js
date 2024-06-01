71bf0d9bee607a8503549f09bb4459a95cbef8de
Diegodenac <diegocanedo917@gmail.com>
2024-06-01 03:18:58 -0400

    refact: Less functions in project, they were moved and adapted to ProjectFeedbackAssigner

--- src/ProjectTestsFeedbackAssigner.js
+++ src/ProjectTestsFeedbackAssigner.js
@@ -1,11 +1,12 @@
 export class ProjectTestsFeedbackAssigner {
-    constructor(percentOfTestInProject){
-        this.assignPoints(percentOfTestInProject);
+    constructor(commitList){
+        this.assignPoints(commitList);
     };
-    assignPoints(percentOfTestInProject) {
-        if (percentOfTestInProject === null) {
-            this.points = 0;
-        } else {
+    assignPoints(commitList) {
+        this.points = 0;
+        let percentOfTestInProject = this.getPercentageOfCommitsWithTests(commitList);
+        if(isFinite(percentOfTestInProject)){
+            this.points = 8;
             if(Number(percentOfTestInProject) > 0.6){
                 this.points = 12;
                 if(Number(percentOfTestInProject) >= 0.8){
@@ -15,11 +16,30 @@ export class ProjectTestsFeedbackAssigner {
                     }
                 }
             }
-            else
-                this.points = 8;
         }
+        
     };
     getPoints() {    
         return this.points;
     };
+    isRefactoringCommit(commit){
+        const refactor = 'refact:';
+        return this.getFirstWordCommit(commit) == refactor ? true : false;
+    };
+    getFirstWordCommit(commit){
+        const index = 0;
+        return commit.getCommitDescription().trim().split(' ')[index];
+    };
+    getPercentageOfCommitsWithTests(commitList) {
+        let count = 0;
+        let nonRefactCommits = 0;
+        for (const commit of commitList){
+          if(!this.isRefactoringCommit(commit)) {
+            nonRefactCommits++;
+            if(commit.getAddedTests().getValue() >= 1) 
+              count ++;
+          }
+        };
+        return count/nonRefactCommits;
+    }
 }
--- src/project.js
+++ src/project.js
@@ -5,21 +5,16 @@ import { ProjectTestsFeedbackAssigner } from "./ProjectTestsFeedbackAssigner.js"
 
 export class Project {
   name = "";
-  commitList = [];
+  
   testCoverage = null;
-  addedTests = null;
 
   constructor(name) {
     this.name = name;
-    this.addedTests = new ProjectTestsFeedbackAssigner(null);
+    this.commitList = [];
   }
 
   getProjectName() {
     return this.name;
-  }
-
-  isEmptyProject() {
-    return this.commitList.length === 0;
   }
 
   addCommit(commitDescription, modifiedLines, addedTests, percentageOfCoverage, commitDay, commitMonth, commitYear, hours = 0, minutes = 0, codeComplexity) {
@@ -39,39 +34,12 @@ export class Project {
   getCodeComplexity() {
       return new ProjectCodeComplexity(this.commitList);
   }
-
+  getAddedTestsPerCommit(){
+    return new ProjectTestsFeedbackAssigner(this.commitList);
+  }
   changeName(newName) {
     this.name = newName;
   }
 
-  getAddedTestsPerCommit(){
-    if (!this.isEmptyProject()){
-      return new ProjectTestsFeedbackAssigner(this.getPercentageOfCommitsWithTests());
-    }
-    else{
-      return this.addedTests;
-    }
-  }
-
-  getPercentageOfCommitsWithTests() {
-    let count = 0;
-    let nonRefactCommits = 0;
-    for (const commit of this.commitList){
-      if(!this.isRefactoringCommit(commit)) {
-        nonRefactCommits++;
-        if(commit.getAddedTests().getValue() >= 1) 
-          count ++;
-      }
-    };
-    return count/nonRefactCommits;
-  }
-
-  isRefactoringCommit(commit){
-    const refactor = 'refact:';
-    return this.getFirstWordCommit(commit) == refactor ? true : false;
-  }
-  getFirstWordCommit(commit){
-    const index = 0;
-    return commit.getCommitDescription().trim().split(' ')[index];
-  }
+  
 }
